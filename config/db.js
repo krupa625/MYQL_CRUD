614df621bdbd46e1f70f05962d2229c5d64a0950
krupa625 <krupapatel1574@gmail.com>
2025-06-06 13:57:59 +0530

    solve connection error

--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
 const { Sequelize } = require('sequelize');
 const { DB_PASS } = require('./config');
+
 const sequelize = new Sequelize('CRUD_db', 'root', DB_PASS, {
-  host: 'localhost',
-  dialect: 'mysql',   
+  host: '127.0.0.1',
+  // host: 'localhost',
+  dialect: 'mysql',
+  
 });
 
 sequelize.authenticate()
-  .then(() => console.log("✅ Database connected"))
-  .catch(err => console.log("❌ Error: " + err));
+  .then(() => console.log("Database connected"))
+  .catch(err => console.log("Error: " + err));
 
 module.exports = sequelize;
